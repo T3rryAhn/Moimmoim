cc956941b844f340691076840abbb7a67069a511
Hwang-Jong-Mun <hjm8539636@naver.com>
2023-05-03 13:37:45 +0900

    jm 현재까지 0503 13 37

--- moimmoim_springBoot_project/out/production/resources/static/js/inputValidate.js
+++
@@ -1,8 +0,0 @@
-var idInput = document.getElementById('idInput');
-idInput.addEventListener("blur", function() {
-
-    var inputValue = idInput.inputValue;
-    var confirmMsg = document.getElementById('confirm-msg').innerHTML(inputValue);
-
-});
-
