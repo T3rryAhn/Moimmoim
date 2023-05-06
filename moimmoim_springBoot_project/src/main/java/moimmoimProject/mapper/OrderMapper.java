43d0eaa358b21ad7fdb6da5fe3764d5afa420874
Terry <t3rryahn.dev@gmail.com>
2023-05-06 18:08:35 +0900

    티케팅 개발중

--- moimmoim_springBoot_project/src/main/java/moimmoimProject/mapper/OrderMapper.java
+++ moimmoim_springBoot_project/src/main/java/moimmoimProject/mapper/OrderMapper.java
@@ -3,13 +3,18 @@ package moimmoimProject.mapper;
 import moimmoimProject.domain.ticketDomain.OrderDo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface OrderMapper {
 
     OrderDo findByUserIdNum(@Param("userIdNum") Long userIdNum);
 
     OrderDo findByMoimNum(@Param("moimNum") Long moimNum);
 
-    String getMaxOrderNum(Long moimNum);
+    //최대 주문 확인용
+//    String getMaxOrderNum(Long moimNum);
+
+    void insertOrder(OrderDo orderDo);
 }
