da5cbdc7f35e595a161dc777098be4efa3028a44
S_H_Lee <dabansa2@naver.com>
2023-05-10 16:03:37 +0900

    모임 작성 페이지 구현

--- moimmoim_springBoot_project/src/main/java/moimmoimProject/controller/moimController/MoimController.java
+++ moimmoim_springBoot_project/src/main/java/moimmoimProject/controller/moimController/MoimController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.Date;
 
 @Controller
 @AllArgsConstructor
