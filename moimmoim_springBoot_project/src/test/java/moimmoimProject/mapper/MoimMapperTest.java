9db0b43eba9a55855f394e836bd73059c8330c2e
S_H_Lee <dabansa2@naver.com>
2023-05-11 10:26:20 +0900

    .gitignore

---
+++ moimmoim_springBoot_project/src/test/java/moimmoimProject/mapper/MoimMapperTest.java
@@ -0,0 +1,13 @@
+package moimmoimProject.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoimMapperTest {
+
+    @Test
+    void test(){
+
+    }
+}
